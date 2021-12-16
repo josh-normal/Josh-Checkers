c6a63e08b2369fa016d3da68edc9294907c0fe67
Josh <mpphuoc@gmail.com>
2021-12-15 23:46:22 -0500

    wireframing

---
+++ script.js
@@ -0,0 +1,38 @@
+/* ------------ Constant ---------- */
+
+
+
+/* ------------ Variable ---------- */
+
+
+
+/* ------------ State ---------- */
+
+
+/* ------------ Event Listener ---------- */
+// checker select
+
+
+// Move select
+
+
+// Reset Button
+
+
+/* ------------ Function ---------- */ 
+// Reset Game
+
+
+// Checker Selection, click 2nd time to un-select
+
+
+// Check Possible Move
+
+
+// Highlight Possible Move
+
+
+// Highlight Player's Turn
+
+
+// 
