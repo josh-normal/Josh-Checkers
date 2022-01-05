bb55f789f692e3e875294eac6b6faabdc0a13900
Josh <mpphuoc@gmail.com>
2022-01-05 17:02:59 -0500

    progressing

--- script.js
+++ script.js
@@ -12,19 +12,20 @@ const turnText = document.querySelector('#subturn1')
 const infoText = document.querySelector('#info')
 
 /* ------------ Variable, Game state ---------- */
-let validRed = document.querySelectorAll('.red');
-let validBlack = document.querySelectorAll('.black');
+
 let playerPieces;
 let currentPlayer = 'red';
 let turn = true;
+let move = false;
+let pieceEl;
 let active = true;
 let winner = false;
 let remainBlack = 12;
 let remainRed = 12;
-let valid = false;
 let pieceProperties = {
     pieceId: -1,
     pieceClass: -1,
+    pieceType: '',
     isKing: false,
     upperLeft: true,
     upperRight: true,
@@ -47,57 +48,121 @@ let lowerRightMove = {
     moveId: -1,
     moveClass: -1,
 };
-
+let board = [
+    ["red", "red", "red", "red"],
+    ["red", "red", "red", "red"],
+    ["red", "red", "red", "red"],
+    ["emp", "emp", "emp", "emp"],
+    ["emp", "emp", "emp", "emp"],
+    ["black", "black", "black", "black"],
+    ["black", "black", "black", "black"],
+    ["black", "black", "black", "black"],
+];
 
 /* ------------ Event Listener ---------- */
 // checker select: black if true and red if false
 // document.querySelector('.game').addEventListener('click', handleClick);
-function giveEventListeners() {
-    if (turn) {
-        for (let i = 0; i < validRed.length; i++) {
-            validRed[i].addEventListener("click", handleClick);
-        }
-    } else {
-        for (let i = 0; i < validBlack.length; i++) {
-            validBlack[i].addEventListener("click", handleClick);
-        }
-    }}
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", handleClick);
+}
 // Reset Button
-document.querySelector('button').addEventListener('click', render);
+document.querySelector('button').addEventListener('click', reset);
 
 
 /* ------------ Function ---------- */ 
 // Reset Game
-function render() {
+function reset() {
     turn = true;
     active = true;
     winner = false;
     remainBlack = 12;
     remainRed = 12;
+    validYes.forEach(function(tdyes) {
+        tdyes.classList.remove("red", "black", "emp")})
+    board = [
+        ["red", "red", "red", "red"],
+        ["red", "red", "red", "red"],
+        ["red", "red", "red", "red"],
+        ["emp", "emp", "emp", "emp"],
+        ["emp", "emp", "emp", "emp"],
+        ["black", "black", "black", "black"],
+        ["black", "black", "black", "black"],
+        ["black", "black", "black", "black"],
+    ];
+    render();
+    highlightTurn();
+}
+
+function render() {
+    validYes.forEach(function(tdyes) {
+        tdyes.classList.add(board[parseInt(tdyes.parentElement.id)][parseInt(tdyes.classList.item(0))])
+        tdyes.style.backgroundImage = "none";
+    })
+    let validRed = document.querySelectorAll('.red');
     validRed.forEach(function(tdred) {
         tdred.style.backgroundImage = "url(./img/redsmall.png)";
     });
+    let validBlack = document.querySelectorAll('.black');
     validBlack.forEach(function(tdblack) {
         tdblack.style.backgroundImage = "url(./img/blacksmall.png)";
     });
-    highlightTurn();
-    giveEventListeners()
+
 }
 
 // Checker Selection, click 2nd time to un-select
 function handleClick(evt) {
-    resetPieceProperties();
-    pieceProperties.pieceId = parseInt(evt.target.parentElement.id);
-    pieceProperties.pieceClass = parseInt(evt.target.classList.item(0));
-    console.log(pieceProperties.pieceId);
-    console.log(pieceProperties.pieceClass);
-    checkForKing(evt);
+    // resetPieceProperties();
+    if (turn) {
+        if (evt.target.classList.item(2) == 'red') {
+            pieceProperties.pieceId = parseInt(evt.target.parentElement.id);
+            pieceProperties.pieceClass = parseInt(evt.target.classList.item(0));
+            pieceProperties.pieceType = evt.target.classList.item(2);
+            pieceEl = evt.target
+            move = true;
+            checkForKing(evt.target)
+        } else {
+            if (pieceProperties.pieceType) {
+                board[parseInt(evt.target.parentElement.id)][parseInt(evt.target.classList.item(0))] = pieceProperties.pieceType
+                move = false;
+                board[pieceProperties.pieceId][pieceProperties.pieceClass] = 'emp'
+                pieceEl.classList.remove("red")
+                console.log(board)
+                resetPieceProperties()
+                changePlayer()
+                render();
+                pieceEl = null;
+            }
+        }
+    } else if (!turn) {
+        if (evt.target.classList.item(2) == 'black') {
+            pieceProperties.pieceId = parseInt(evt.target.parentElement.id);
+            pieceProperties.pieceClass = parseInt(evt.target.classList.item(0));
+            pieceProperties.pieceType = evt.target.classList.item(2);
+            pieceEl = evt.target
+            move = true;
+            checkForKing(evt.target)
+        } else {
+            if (pieceProperties.pieceType) {
+                board[parseInt(evt.target.parentElement.id)][parseInt(evt.target.classList.item(0))] = pieceProperties.pieceType
+                move = false;
+                board[pieceProperties.pieceId][pieceProperties.pieceClass] = 'emp'
+                pieceEl.classList.remove("black")
+                console.log(board)
+                resetPieceProperties()
+                changePlayer()
+                render();
+                pieceEl = null;
+            }
+        }
+    }
 }
+
 
 function resetPieceProperties() {
     pieceProperties.pieceId = -1;
     pieceProperties.pieceClass = -1;
     pieceProperties.isKing = false;
+    pieceProperties.pieceType = '';
     pieceProperties.upperLeft = true;
     pieceProperties.upperRight = true;
     pieceProperties.lowerLeft = true;
@@ -113,90 +178,80 @@ function resetPieceProperties() {
     lowerRightMove.moveClass = -1;
 }
 
-function checkForKing(evt) {
-    if (evt.target.classList.contains("king")) {
+
+function checkForKing(event) {
+    if (event.classList.contains("king")) {
         pieceProperties.isKing = true;
     } else {
         pieceProperties.isKing = false;
     }
-    checkPossibleMove();
+    checkPossibleMove(event);
 }
 
 
 // Check Possible Move
-function checkPossibleMove() {
-    if (turn && !pieceProperties.isKing) {
-        pieceProperties.upperLeft = false;
-        pieceProperties.upperRight = false;
-    } else if (!turn && !pieceProperties.isKing) {
-        pieceProperties.lowerLeft = false;
-        pieceProperties.lowerRight = false;
-    }
-    
+function checkPossibleMove(event) {
     if (pieceProperties.pieceId == 0) {
-        pieceProperties.upperLeft = false;
-        pieceProperties.upperRight = false;
+        pieceProperties.upperLeft = false
+        pieceProperties.upperRight = false
     } else if (pieceProperties.pieceId == 7) {
-        pieceProperties.lowerLeft = false;
-        pieceProperties.lowerRight = false;
+        pieceProperties.lowerLeft = false
+        pieceProperties.lowerRight = false
     }
 
-    if (pieceProperties.pieceClass == 0) {
-        pieceProperties.upperLeft = false;
-        pieceProperties.lowerLeft = false;
-    } else if (pieceProperties.pieceClass == 7) {
-        pieceProperties.upperRight = false;
-        pieceProperties.lowerRight = false;
+    if (pieceProperties.pieceClass == 0 && (pieceProperties.pieceId % 2 === 0)) {
+        pieceProperties.upperLeft = false
+        pieceProperties.lowerLeft = false
+    } else if (pieceProperties.pieceClass == 3 && (pieceProperties.pieceId % 2 !== 0)) {
+        pieceProperties.upperRight = false
+        pieceProperties.lowerRight = false
     }
 
-    console.log(pieceProperties)
+    if (turn) {
+        if (!pieceProperties.isKing) {
+            pieceProperties.upperLeft = false
+            pieceProperties.upperRight = false
+        }
+    } else {
+        if (!pieceProperties.isKing) {
+            pieceProperties.lowerLeft = false
+            pieceProperties.lowerRight = false
+        }
+    }
 
-    if (pieceProperties.upperLeft) {
-        upperLeftMove.moveId = pieceProperties.pieceId - 1;
-        upperLeftMove.moveClass = pieceProperties.pieceClass - 1;
+    if (turn) {
         
-        // if (turn) {
-        //     validRed.forEach(function(tdred) {
-        //         if (tdred.parentElement.id == upperLeftMove.moveId && tdred.classList.item(0) == upperLeftMove.moveClass) {
-        //             pieceProperties.upperLeft = false;
-        //             exit();
-        //         }
-        //     });
-        //     validBlack.forEach(function(tdblack) {
-        //         if (tdblack.parentElement.id == upperLeftMove.moveId && tdblack.classList.item(0) == upperLeftMove.moveClass) {
-        //             pieceProperties.upperLeft = false;
-        //             exit();
-        //         }
-        //     });
-        // } else {
-        //     validBlack.forEach(function(tdblack) {
-        //         if (tdblack.parentElement.id == this.moveid) {
-        //             if (tdblack.classList.item(0) == this.moveclass) {
-        //                 this.prop = false;
-        //             }
-        //         }
-        //     });
-        // }
-    };
-    if (pieceProperties.upperRight) {
-        upperRightMove.moveId = pieceProperties.pieceId - 1;
-        upperRightMove.moveClass = pieceProperties.pieceClass + 1;
-        
-    };
-    if (pieceProperties.lowerLeft) {
-        lowerLeftMove.moveId = pieceProperties.pieceId + 1;
-        lowerLeftMove.moveClass = pieceProperties.pieceClass - 1;
-        
-    };
-    if (pieceProperties.lowerRight) {
-        lowerRightMove.moveId = pieceProperties.pieceId + 1;
-        lowerRightMove.moveClass = pieceProperties.pieceClass + 1;
-        
-    };
+    }
 
-    console.log(pieceProperties)
+
+    validatePossibleMove();
 }
 
+// let upperLeftMove = {
+//     moveId: -1,
+//     moveClass: -1,
+// };
+// let upperRightMove = {
+//     moveId: -1,
+//     moveClass: -1,
+// };
+// let lowerLeftMove = {
+//     moveId: -1,
+//     moveClass: -1,
+// };
+// let lowerRightMove = {
+//     moveId: -1,
+//     moveClass: -1,
+// };
+
+
+function validatePossibleMove() {
+    
+
+
+
+    highlightMove();
+}
 
 // Highlight Possible Move
 function highlightMove() {
@@ -213,6 +268,7 @@ function checkForWin() {
         turnText.textContent = "Win";
         infoText.style.color = "red";
         infoText.textContent = "Red Win";
+        winner = true;
     } else if (remainRed === 0) {
         subred.style.borderWidth = "0px";
         subblack.style.borderWidth = "10px";
@@ -220,8 +276,11 @@ function checkForWin() {
         turnText.textContent = "Win";
         infoText.style.color = "black";
         infoText.textContent = "Black Win";
+        winner = true;
     }
-    changePlayer();
+    if (!winner) {
+        changePlayer();
+    }
 }
 
 
@@ -249,9 +308,8 @@ function changePlayer() {
     } else {
         turn = true;
     }
-    giveEventListeners();
+    highlightTurn();
 }
 
 
-// render();
-// giveEventListeners();
+render();
